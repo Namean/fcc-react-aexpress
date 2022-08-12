8a5b4db6c99e8f1e8bb911e55f97e363dff295e3
Namean <dwilsoncnc@gmail.com>
2022-08-12 18:02:46 -0400

    Adding the working source files to repository

---
+++ api/routes/testAPI.js
@@ -0,0 +1,10 @@
+// testAPI.js
+
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function (req, res, next) {
+  res.send("API is working properly");
+});
+
+module.exports = router;
